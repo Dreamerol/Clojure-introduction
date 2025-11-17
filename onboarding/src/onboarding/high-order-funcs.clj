f6b07eebc77e28a61a408cae191b1ecce15319b5
Mihaela Koseva <mihaela.koseva@zigiwave.com>
2025-11-17 13:41:03 +0200

    Tasks solved with thread macro - 11.17.2025

--- onboarding/src/onboarding/high-order-funcs.clj
+++ onboarding/src/onboarding/high-order-funcs.clj
@@ -23,7 +23,19 @@
                 acc)))
           0 els))
 
-(find-oldest-gamer players)
+;using macro
+(defn find-oldest-gamer-macro
+  [els]
+  (reduce (fn [acc item]
+            (let [currAge (get item "age")]
+              (->
+               (> currAge acc)
+               (if
+                currAge
+                 acc)))) 0 els))
+
+
+(find-oldest-gamer-macro players)
 
 ;find the average age of gamers-------------------------------------------------------------------------------------------
 (defn average-age-gamers
@@ -33,8 +45,28 @@
         countPlayers (count players)]
     (/ sumAges countPlayers)))
 ;;and then we calculate the result with the local bindings
-
 (average-age-gamers players)
+
+;;using thread - macro
+(defn average-age-gamers-macro
+  [players]
+  (let [sumAges (reduce (fn [acc item]
+                          (->
+
+                           (get item "age")
+                           (#(do (prn %) %))
+
+                           (+ acc))) 0 players)
+        ;; (-> item
+        ;;     (get "age")
+        ;;     (+ acc)) another version
+
+        countPlayers (count players)]
+    (/ sumAges countPlayers)))
+;;and then we calculate the result with the local bindings
+players
+(average-age-gamers-macro players)
+
 
 ;find all games-------------------------------------------------------------------------------------------------------------
 (defn allGamesReduce
@@ -43,8 +75,38 @@
             (concat acc (get item "ownedGames")))
           [] els))
 
+;;thread macro 
+(defn allGamesReduce-macro
+  [els]
+  (reduce (fn [acc item]
+            (-> item
+                (get "ownedGames")
+                (concat acc)))
+          [] els))
+
+(allGamesReduce-macro players)
 (def all-games (allGamesReduce players))
 all-games
+;create collection of all game names
+(defn create-list-all-game-names
+  [els]
+  (reduce (fn [acc item]
+            (conj acc (get item "name")))
+          #{} els))
+
+;with macro !
+(defn create-list-all-game-names-macro
+  [els]
+  (reduce (fn [acc item]
+            (->>
+             (get item "name")
+             (conj acc)))
+          #{} els))
+
+(create-list-all-game-names-macro all-games)
+
+(create-list-all-game-names all-games)
+
 ;transforming the all-games structure
 (defn remove-keys
   [els key]
@@ -52,6 +114,18 @@ all-games
             (conj acc (assoc {} (get item "name") (get item key))))
           [] els))
 
+;thread macro 
+(defn remove-keys-macro
+  [els key]
+  (reduce (fn [acc item]
+            (->>
+             (assoc {} (get item "name") (get item key))
+             (conj acc)))
+          [] els))
+
+(remove-keys-macro all-games "timePlayed")
+
+;;find the games played for over 150 hours-------------------------------------------------------------------------------------------------------------
 (def formated-games-timePlayed (remove-keys all-games "timePlayed"))
 ;formating from [{..}{...}{...}] -> {.....}
 ;?why into doesnt keep the same keys
@@ -68,20 +142,56 @@ formated-games-timePlayed
   (reduce (fn [acc item]
             (let [key (first (keys item)) points (first (vals item))]
               (assoc acc key (+ (or (get acc key) 0) points)))) {} els))
+
+(defn convert-in-map-new-vals-macro
+  [els]
+  (reduce (fn [acc item]
+            (let [key (first (keys item)) points (first (vals item)) p (or (get acc key) 0)]
+              (->> p 
+               (+ points) 
+                   (assoc acc key)
+               ))) {} els))
+
+
+(convert-in-map-new-vals-macro formated-games-timePlayed)
 (def final-games-timeplayed-value (convert-in-map-new-vals formated-games-timePlayed))
 final-games-timeplayed-value
 
 
 ;filter games played > 150 hours
 ;help function to merge the values
-(defn sum-hours-assoc
+(defn sum-hours-assoc-with-destructing
   [els]
-  (reduce (fn [acc item];;tuk
-            (let [key (first item) points (second item)];destructure in func args 
-              (assoc acc key (+ (or (get acc key) 0) points))
-              ;; (prn (or (get acc key) 0))
-              ;;   )
-              )){} els))
+  (reduce (fn [acc [key points]];;tuk
+            (assoc acc key (+ (or (get acc key) 0) points)))
+
+          {} els))
+(sum-hours-assoc-with-destructing final-games-timeplayed-value)
+
+;macro
+(defn sum-hours-assoc-with-destructing-macro
+  [els]
+  (reduce (fn [acc [key points]];;tuk
+            (->
+             (get acc key)
+             (or 0)
+             (->> 
+              (+ points)
+              (assoc acc key))
+            ) 
+
+        )  {} els))
+
+(defn sum-hours-assoc-with-destructing-macro-better
+  [els]
+  (reduce (fn [acc [key points]]
+            (let [p (or (get acc key) 0)]
+              (->> p
+               (+ points)
+               (assoc acc key))))  {} els))
+
+(sum-hours-assoc-with-destructing-macro-better final-games-timeplayed-value)
+(sum-hours-assoc-with-destructing-macro final-games-timeplayed-value)
 
 ;?? destructing howwww
 ;; (defn sum-hours-assoc
@@ -92,12 +202,20 @@ final-games-timeplayed-value
 ;;             ;;   )
 ;;             ){} els))
 
-(def final-final-games-timeplayed (sum-hours-assoc final-games-timeplayed-value))
+(def final-final-games-timeplayed (sum-hours-assoc-with-destructing final-games-timeplayed-value))
 final-final-games-timeplayed
 
 (def filered-games (filter (fn [item]
                              (> (second item) 150)) final-final-games-timeplayed))
+
+;macro
+(def filered-games-macro (filter (fn [item]
+                                   (->
+                                    (second item)
+                                    (> 150))) final-final-games-timeplayed))
+
 filered-games
+filered-games-macro
 
 (defn combine-games-over-150
   [els]
@@ -108,19 +226,7 @@ filered-games
 (def final-filterd-games-above150 (combine-games-over-150 filered-games))
 final-filterd-games-above150
 
-;;find least popular game------------------------------------------------------------------------
-(defn find-least-popular-game-with-name
-  [els]
-  (reduce (fn [acc item]
-            (let [hours (second item) hoursmin (second acc)]
-              (if (> hoursmin hours)
-                item
-                acc)))  (first els) els))
-
-(def name-of-least-popular-game (first (find-least-popular-game-with-name final-games-timeplayed-value)))
-name-of-least-popular-game
-
-;find the cheapest game
+;find the cheapest game----------------------------------------------------------------------------
 
 (defn cheapest-game
   [els]
@@ -134,6 +240,76 @@ name-of-least-popular-game
 name-cheapest-game
 ;name-cheapest-game
 
+; find the total playtime of each person-----------------------------------------------------------
+players
+(defn calculate-playtime-person
+  [els]
+  (reduce (fn [acc item]
+            (+ acc (get item "timePlayed")))
+          0 els))
+
+(defn calculate-playtime-for-all [els]
+  (reduce (fn [acc item]
+            (let [name (get item "name") playtime (calculate-playtime-person (get item "ownedGames"))]
+              (assoc acc name playtime)))  {} els))
+
+(calculate-playtime-for-all players)
+;;with macro
+(defn calculate-playtime-person-macro
+  [els]
+  (reduce (fn [acc item]
+            (-> (get item "timePlayed")
+                (+ acc)))
+          0 els))
+
+(defn calculate-playtime-for-all-macro [els]
+  (reduce (fn [acc item]
+            (let [name (get item "name") playtime (calculate-playtime-person-macro (get item "ownedGames"))]
+              (assoc acc name playtime)))  {} els));;tuk maj e samo edna finkciq i nqma nugda ot macro
+
+; find the total playtime of everyone-----------------------------------------------------------
+(defn find-total-playtime-of-everyone
+  [els]
+  (reduce (fn [acc item]
+            (+ acc (get item "timePlayed")))
+          0 els))
+
+;;thread macro
+(defn find-total-playtime-of-everyone-macro
+  [els]
+  (reduce (fn [acc item]
+            (-> item
+                (get "timePlayed")
+                (+ acc)))
+          0 els))
+
+all-games
+(find-total-playtime-of-everyone-macro all-games)
+;total playtime of everyone
+(find-total-playtime-of-everyone all-games)
+
+;;find least popular game------------------------------------------------------------------------
+(defn find-least-popular-game-with-name
+  [els]
+  (reduce (fn [acc item]
+            (let [hours (second item) hoursmin (second acc)]
+              (if (> hoursmin hours)
+                item
+                acc)))  (first els) els))
+
+(defn find-least-popular-game-with-name-macro
+  [els]
+  (reduce (fn [acc item]
+            (let [hours (second item) hoursmin (second acc)]
+              (-> (> hoursmin hours)
+                  (if
+                   item
+                    acc)))) (first els) els))
+
+
+(def name-of-least-popular-game (first (find-least-popular-game-with-name final-games-timeplayed-value)))
+name-of-least-popular-game
+
 ;find most expensive game---------------------------------------------------------------------
 (defn most-expensive-game
   [els]
@@ -142,18 +318,43 @@ name-cheapest-game
               (if (< accPrice currentPrice)
                 item
                 acc))) (first els) els))
-(most-expensive-game all-games)
+;macro
+(defn most-expensive-game-macro
+  [els]
+  (reduce (fn [acc item]
+            (let [accPrice (get acc "price") currentPrice (get item "price")]
+              (->
+               (< accPrice currentPrice)
+               (if
+                item
+                 acc)))) (first els) els))
+
+(most-expensive-game-macro all-games)
 
 (def name-most-expensive-game (get (most-expensive-game all-games) "name"))
 name-most-expensive-game
 
 ;find the total money spent on each game--------------------------------------------------------------------------------
-(def formated-games-price(remove-keys all-games "price"))
+(def formated-games-price (remove-keys all-games "price"))
 formated-games-price
 
 ;finding the sum of money spend on each game
 (def total-money-spend-on-game (convert-in-map-new-vals formated-games-price))
 total-money-spend-on-game
+
+(defn foo [[arg & others :as all]]
+  (prn arg others all))
+
+(foo [1 2 3])
+
+(defn doo [{kaval :a maval "b" :as all}]
+  (let []
+    (prn all kaval maval)))
+
+
+(doo {:a 1 "b" 3 :c "adwa"})
+
+
 
 ;;find the price/timeplayed average of each game-------------------------------------------------------------------------
 all-games
@@ -165,6 +366,17 @@ all-games
                            0 els) count-games (count els)]
     (/ sum-prices count-games)))
 
+;;thread macro
+(defn find-average-attribute-games-macro
+  [els key]
+  (let [sum-prices (reduce (fn [acc item]
+                             (->
+                              (get item key)
+                              (+ acc)))
+                           0 els) count-games (count els)]
+    (/ sum-prices count-games)))
+(find-average-attribute-games-macro all-games "price")
+
 (def average-price (find-average-attribute-games all-games "price"))
 average-price
 
@@ -172,16 +384,3 @@ average-price
 average-time-played
 
 
-; find the total playtime of each person-----------------------------------------------------------
-
-
-; find the total playtime of everyone-----------------------------------------------------------
-(defn find-total-playtime-of-everyone
-  [els]
-  (reduce (fn [acc item]
-            (+ acc (get item "timePlayed")))
-          0 els))
-
-all-games
-;total playtime of everyone
-(find-total-playtime-of-everyone all-games)
